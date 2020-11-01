7cae23ffebb839aa049f1333146341f6c172fd84
Aaron Lauer <lauer.aaron@gmail.com>
2020-11-01 21:08:08 +0800

    final assignment

--- r-programming/assignment 1/complete.R
+++ r-programming/assignment 1/complete.R
@@ -1,59 +1,29 @@
-# PROGRAMMING ASSIGMENT 1: AIR POLLUTION
-# PART 2
-# Write a function that reads a directory full of files and reports the number of 
-# completely observed cases in each data file. The function should return a data 
-# frame where the first column is the name of the file and the second column is 
-# the number of complete cases. A prototype of this function follows
-
-
-complete <- function(directory, id = 1:332) {
-    # Get full path of the specsdata folder
-    directory <- paste(getwd(),"/","specdata","/",sep="")
+complete <- function(directory, id = 1:332){
+    ## 'director' is a character vector of length 1 indicating
+    ## the location of the CSV files
     
-    # Aux variables
-    file_list <- list.files(directory)
-    ids <- vector()
-    nobs <- vector()
+    ## 'id' is an integer vector indicating the monitor ID numbers
+    ## to be used
     
-    #For each id passed as parameter:
-    for (i in id) {
-        # Read the file,
-        file_dir <- paste(directory,file_list[i],sep="")
-        file_data <- read.csv(file_dir)
-        
-        # acumulate ids and nobs values in the vectors    
-        ids = c(ids,i)
-        nobs = c(nobs,sum(complete.cases(file_data)))        
+    ## Return a data frame of the from:
+    ## id nobsS
+    ## 1  117
+    ## 2  1041
+    ## ...
+    ## where 'id' is the monitor ID number and 'nobs' is the
+    ## number of complete cases
+    results <- data.frame(id=numeric(0), nobs=numeric(0))
+    for(monitor in id){
+        path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+        monitor_data <- read.csv(path)
+        interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
+        interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+        nobs <- nrow(interested_data)
+        results <- rbind(results, data.frame(id=monitor, nobs=nobs))
     }
-    # Finally, Create the data frame using the vectors and return it
-    data.frame(id = ids, nobs = nobs)
+    results
 }
-
-
-# >============== TEST SCENARIOS ==============<
-
-test1 <- complete("specdata", 1)
-test1
-##   id nobs
-## 1  1  117
-test2 <- complete("specdata", c(2, 4, 8, 10, 12))
-test2
-##   id nobs
-## 1  2 1041
-## 2  4  474
-## 3  8  192
-## 4 10  148
-## 5 12   96
-test3 <- complete("specdata", 30:25)
-test3
-##   id nobs
-## 1 30  932
-## 2 29  711
-## 3 28  475
-## 4 27  338
-## 5 26  586
-## 6 25  463
-test4 <- complete("specdata", 3)
-test4
-##   id nobs
-## 1  3  243
+complete("specdata", 1)
+complete("specdata", c(2, 4, 8, 10, 12))
+complete("specdata", 30:25)
+complete("specdata", 3)
--- r-programming/assignment 1/corr.R
+++ r-programming/assignment 1/corr.R
@@ -1,66 +1,56 @@
-# PROGRAMMING ASSIGMENT 1: AIR POLLUTION
-# PART 3
-# Write a function that takes a directory of data files and a threshold for 
-# complete cases and calculates the correlation between sulfate and nitrate for 
-# monitor locations where the number of completely observed cases (on all 
-# variables) is greater than the threshold. The function should return a vector 
-# of correlations for the monitors that meet the threshold requirement. If no
-# monitors meet the threshold requirement, then the function should return a 
-# numeric vector of length 0.
-
-source("complete.R")
-
-corr <- function(directory, threshold = 0) {
-    # Get full path of the specsdata folder
-    directory <- paste(getwd(),"/",directory,"/",sep="")    
+corr <- function(directory, threshold = 0){
+    ## 'directory' is a character vector of length 1 indicating
+    ## the location of the CSV files
     
-    #Get observations and filter by threshold
-    observations <- complete(directory)
-    filtered_observations = subset(observations,observations$nobs > threshold)
-        
-    # Aux variables
-    file_list <- list.files(directory)
-    correlation <- vector()
+    ## 'threshold' is a numeric vector of length 1 indicating the 
+    ## number of completely observed observations (on all
+    ## variables) requi?red to compute the correlation between
+    ## nitrate and sulfate; the default is 0
     
-    # For each id in filtered observations:
-    for (i in filtered_observations$id) {
-        # Read the file,
-        file_dir <- paste(directory,file_list[i],sep="")
-        file_data <- read.csv(file_dir)
-        # remove NA,
-        file_data <- subset(file_data,complete.cases(file_data))        
-        # and calculate the cor and accumulate it in the corellation vector.
-        correlation <- c(correlation,cor(file_data$nitrate,file_data$sulfate))    
+    ## Return a numeric vector of correlations
+    ## NOTE: Do not round the result!
+    cor_results <- numeric(0)
+    
+    complete_cases <- complete(directory)
+    complete_cases <- complete_cases[complete_cases$nobs>=threshold, ]
+    #print(complete_cases["id"])
+    #print(unlist(complete_cases["id"]))
+    #print(complete_cases$id)
+    
+    if(nrow(complete_cases)>0){
+        for(monitor in complete_cases$id){
+            path <- paste(getwd(), "/", directory, "/", sprintf("%03d", monitor), ".csv", sep = "")
+            #print(path)
+            monitor_data <- read.csv(path)
+            #print(monitor_data)
+            interested_data <- monitor_data[(!is.na(monitor_data$sulfate)), ]
+            interested_data <- interested_data[(!is.na(interested_data$nitrate)), ]
+            sulfate_data <- interested_data["sulfate"]
+            nitrate_data <- interested_data["nitrate"]
+            cor_results <- c(cor_results, cor(sulfate_data, nitrate_data))
+        }
     }
-    #Finally, return the vector
-    correlation
+    cor_results
 }
 
-# >============== TEST SCENARIOS ==============<
-
-#source("corr.R")
-#source("complete.R")
+source("corr.R")
+source("complete.R")
 cr <- corr("specdata", 150)
 head(cr)
-## [1] -0.01896 -0.14051 -0.04390 -0.06816 -0.12351 -0.07589
+
 summary(cr)
-##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-## -0.2110 -0.0500  0.0946  0.1250  0.2680  0.7630
+
 cr <- corr("specdata", 400)
 head(cr)
-## [1] -0.01896 -0.04390 -0.06816 -0.07589  0.76313 -0.15783
+
 summary(cr)
-##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-## -0.1760 -0.0311  0.1000  0.1400  0.2680  0.7630
+
 cr <- corr("specdata", 5000)
 summary(cr)
-##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-## 
+
 length(cr)
-## [1] 0
+
 cr <- corr("specdata")
 summary(cr)
-##    Min. 1st Qu.  Median    Mean 3rd Qu.    Max. 
-## -1.0000 -0.0528  0.1070  0.1370  0.2780  1.0000
+
 length(cr)
-## [1] 323
